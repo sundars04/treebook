91a83a13c8bfda3327c295112de1e100e572c6f5
Sundars0 <Sundars04@outlook.com>
2016-11-17 07:06:51 +0000

    add sign in and sign up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new', as: :signup
+    get 'signin', to: 'devise/sessions#new', as: :signin    
+  end
+
   resources :statuses
   root :to => 'statuses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
